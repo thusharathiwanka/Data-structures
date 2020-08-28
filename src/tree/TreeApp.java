38b13b81e315f733bf7adb4d5bd56920f0133662
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-08-28 22:59:51 +0530

    tree main added

--- src/tree/TreeApp.java
+++ src/tree/TreeApp.java
@@ -2,6 +2,12 @@ package tree;
 
 public class TreeApp {
     public static void main(String[] args) {
+        Tree tree = new Tree();
 
+        tree.insert(1, 10);
+        tree.insert(2, 20);
+        tree.insert(3, 30);
+        tree.insert(4, 40);
+        tree.insert(5, 50);
     }
 }
